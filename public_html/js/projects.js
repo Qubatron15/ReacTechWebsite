c6d4b6471c81082d20160d088ab23bb7694f2c9c
qubatron15 <qubatron15@gmail.com>
2018-11-18 18:28:30 +0100

    First list element background changing experiment

---
+++ public_html/js/projects.js
@@ -0,0 +1,29 @@
+function logSth() {
+    console.log('hahahaha');
+}
+
+let projectsBackgroundElement = document.querySelector("#projects-background"),
+    projectsTitleElement = document.querySelector("#projects-hiding-title");
+
+function resetProjectsBackground() {
+    
+//    projectsBackgroundElement.style.backgroundImage = "url('img/projects-background.jpg')"; 
+    projectsBackgroundElement.style.opacity = "0";
+    projectsTitleElement.style.opacity = "1";
+}
+
+function setProjectsBackground() {
+    projectsBackgroundElement.style.backgroundImage = "url('img/mikroskop.jpg')"; 
+    projectsBackgroundElement.style.opacity = "1";
+    projectsTitleElement.style.opacity = "0";
+}
+
+document.querySelector('#reatkory').addEventListener("mouseenter", (event) => {
+    console.log('enterrrrrr!!!', event);
+    setProjectsBackground();
+});
+
+document.querySelector('#reatkory').addEventListener("mouseleave", (event) => {
+    console.log('leave', event);
+    resetProjectsBackground();
+});
