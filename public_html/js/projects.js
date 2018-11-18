5ac8866eb9d3647629a2035161551db313ef2e22
qubatron15 <qubatron15@gmail.com>
2018-11-19 00:45:38 +0100

    Projects working background changing

--- public_html/js/projects.js
+++ public_html/js/projects.js
@@ -8,29 +8,42 @@ const backgroundImagesConfig = [
     'microscope',
     'fire',
     'tubing'
-]
+];
 
-let projectsBackgroundElement = document.querySelector("#projects-background"),
-    projectsTitleElement = document.querySelector("#projects-hiding-title");
+let allTilesArray = document.querySelectorAll('#projects__tiles__list li'),
+        tilesContainer = document.querySelector('#projects__tiles__list'),
+        projectsBackgroundElement = document.querySelector("#projects-background"),
+        projectsTitleElement = document.querySelector("#projects-hiding-title");
 
-function resetProjectsBackground() {
-    projectsBackgroundElement.style.backgroundImage = "url('img/projects-background.jpg')"; 
-//    projectsBackgroundElement.style.opacity = "0";
-    projectsTitleElement.style.opacity = "1";
-}
 
-function setProjectsBackground() {
-    projectsBackgroundElement.style.backgroundImage = "url('img/mikroskop.jpg')"; 
-//    projectsBackgroundElement.style.opacity = "1";
+function setProjectsBackground(backgroundImage) {
+    projectsBackgroundElement.style.backgroundImage = `url('img/${backgroundImage}.jpg')`;
     projectsTitleElement.style.opacity = "0";
 }
 
-document.querySelector('#reatkory').addEventListener("mouseenter", (event) => {
-    console.log('enterrrrrr!!!', event);
-    setProjectsBackground();
+function resetProjectsBackground() {
+    projectsBackgroundElement.style.backgroundImage = "url('img/projects-background.jpg')";
+    projectsTitleElement.style.opacity = "1";
+}
+
+function isHovered(element) {
+    return element.parentElement.querySelector(':hover') === element;
+}
+
+allTilesArray.forEach((tile, index) => {
+    tile.addEventListener("mouseenter", () => {
+        setTimeout(() => {
+            if(isHovered(tile)) {
+                setProjectsBackground(backgroundImagesConfig[index]);
+            }
+        }, 500);
+    });
 });
 
-document.querySelector('#reatkory').addEventListener("mouseleave", (event) => {
-    console.log('leave', event);
-    resetProjectsBackground();
+tilesContainer.addEventListener("mouseleave", () => {
+    setTimeout(() => {
+        if(!isHovered(tilesContainer)) {
+            resetProjectsBackground();
+        }
+    }, 500);
 });
