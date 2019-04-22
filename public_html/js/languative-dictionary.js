73f01df65b8567a4153e1f94dd9b50f8ad3d6c70
qubatron15 <qubatron15@gmail.com>
2019-04-23 00:05:55 +0200

    Support translation in navigation

--- public_html/js/languative-dictionary.js
+++ public_html/js/languative-dictionary.js
@@ -1,9 +1,15 @@
 languative.modifyDictionary("pl", {
     languageSelectorActualLanguage: "PL",
+
+    navigationAbout: 'o nas',
+    navigationProducts: 'produkty',
     navigationContact: "kontakt",
 });
 
 languative.modifyDictionary("en", {
     languageSelectorActualLanguage: "EN",
+    
+    navigationAbout: 'about',
+    navigationProducts: 'products',
     navigationContact: "contact"
 });
